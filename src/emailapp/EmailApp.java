0f86aaff9963ade0f96f5c64b25db25e31d7a36f
HanhDinh1 <ms.myhanh89@gmail.com>
2023-05-04 17:10:05 -0400

    First commit, almost done

---
+++ src/emailapp/EmailApp.java
@@ -0,0 +1,13 @@
+package emailapp;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+		Email em1 = new Email("John", "Smith");
+		
+		em1.setAlternateEmail("hanh@gmail.com");
+		System.out.println(em1.getAlternateEmail());
+		
+	}
+
+}
